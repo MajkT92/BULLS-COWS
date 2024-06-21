d62a2f9d7dad4f8e8a125cb49ec0a30e221d37ed
MajkT92 <124393552+MajkT92@users.noreply.github.com>
2024-06-21 12:00:07 +0200

    Add files via upload

--- Bull&Cows.py
+++ Bull&Cows.py
@@ -9,12 +9,16 @@ import random
 
 cara = 47 * "-"
 
-#úvodní text
-print("Hi there!")
-print(cara)
-print("I've generated a random 4 digit number for you.")
-print("Let's play a bulls and cows game.")
-print(cara)
+#zdravící už. funkce
+def hello_user():
+    print("Hi there!")
+    print(cara)
+    print("I've generated a random 4 digit number for you.")
+    print("Let's play a bulls and cows game.")
+    print(cara)
+
+#volání funkce
+hello_user()
 
 #funkce na generování čísel
 def generovani_cisel():
@@ -24,60 +28,57 @@ def generovani_cisel():
 
 nahodne_cislo = generovani_cisel()
 
-print(f"Random number (for debugging): {nahodne_cislo}")
-
-while True:
-
-    #vstup uživatele
-    zadane_cislo = input("Enter a number: ")
-    print(cara)
+#uži. funkce pro zadání vstupu uživatelem
+#uži. funkce pro zadání vstupu uživatelem
+def vstup_uzivatele():
+    while True:
+        zadane_cislo = input("Enter a number: ")
+        print(cara)
 
     #ověření zda uživatel zadal pouze 4 znaky nebo písmeno
-    if len(zadane_cislo) != 4 or not zadane_cislo.isdigit():
-        print("Enter only 4 digit number!")
-        continue
+        if len(zadane_cislo) != 4 or not zadane_cislo.isdigit():
+            print("Enter only a 4-digit number!")
+            continue
 
-    #ověření zda uživatel nezadal duplicitní hodnoty
-    if len(set(zadane_cislo)) != 4:
-        print("Entered numeric duplicate numbers!")
-        continue
+        # ověření zda uživatel nezadal duplicitní hodnoty
+        if len(set(zadane_cislo)) != 4:
+            print("Entered number contains duplicate digits!")
+            continue
 
-    #ověření zda uživatel nezadal nulu
-    if zadane_cislo[0] == "0":
-        print("It must not start with 0!")
-        continue
+        # ověření zda uživatel nezadal nulu na začátku
+        if zadane_cislo[0] == "0":
+            print("The number must not start with 0!")
+            continue
 
+        return zadane_cislo
+
+#uži. funkce na počítání Bull & Cows
+def pocet_bulls_cows(spravny_vystup_uzivatele, nahodne_cislo):
     bulls = 0
     cows = 0
 
-
-    #for ověření počet bull & cow
+#for loop pro ověření počtu bull & cow
     for i in range(4):
-        if zadane_cislo[i] == str(nahodne_cislo)[i]:
+        if spravny_vystup_uzivatele[i] == str(nahodne_cislo)[i]:
             bulls += 1
 
-        elif zadane_cislo[i] in str(nahodne_cislo)[i]:
+        elif spravny_vystup_uzivatele[i] in str(nahodne_cislo)[i]:
             cows += 1
 
-    #ověření množství
-    if bulls == 1:
-        pocet_bull = "bull"
-    else:
-        pocet_bull = "bulls"
-
-    if cows == 1:
-        pocet_cow = "cow"
-    else:
-        pocet_cow = "cows"
-
-
-    #výsledá hláška
+#ověření jednotného / množného čísla bull & cow
+    pocet_bull = "bull" if bulls == 1 else "bulls"
+    pocet_cow = "cow" if cows == 1 else "cows"
     print(f"{bulls} {pocet_bull}, {cows} {pocet_cow}")
 
+    return bulls
 
-    #oznamání o vítěství
+#while loop samotné hry
+
+while True:
+    spravny_vystup_uzivatele = vstup_uzivatele()
+    bulls = pocet_bulls_cows(spravny_vystup_uzivatele,nahodne_cislo)
+
     if bulls == 4:
-        print("Correct, you´ve guessed the right number in 4 guesses!")
+        print("Correct, you’ve guessed the right number!")
         print(cara)
-        break
-
+    break
